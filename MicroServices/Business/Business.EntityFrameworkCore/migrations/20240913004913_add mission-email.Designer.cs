e1ec0925310ec256a710b3561b0c59c6cf7f533e
Jackwio <will582145@gmail.com>
2024-09-13 08:54:35 +0800

    Mission 增加email 項 migration

--- MicroServices/Business/Business.EntityFrameworkCore/migrations/20240913004913_add mission-email.Designer.cs
+++ MicroServices/Business/Business.EntityFrameworkCore/migrations/20240913004913_add mission-email.Designer.cs
@@ -4,6 +4,7 @@ using Business.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Business.migrations
 {
     [DbContext(typeof(BusinessDbContext))]
-    partial class BusinessDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240913004913_add mission-email")]
+    partial class addmissionemail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MicroServices/Business/Business.EntityFrameworkCore/migrations/20240913004913_add mission-email.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Business.migrations
+{
+    /// <inheritdoc />
+    public partial class addmissionemail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Mission",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Mission");
+        }
+    }
+}
