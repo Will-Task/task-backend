209a84f9203e140266e839b0a701f8520500b15a
Jackwio <will582145@gmail.com>
2024-12-24 00:59:34 +0800

    1. 調整多國語系文字 2. 增加完成度報告內部的父任務完成度進行計算 => 子任務是否全部完成

--- MicroServices/Business/Business.Application/ReportManagement/ReportAppService.cs
+++ MicroServices/Business/Business.Application/ReportManagement/ReportAppService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Business.FileManagement.Dto;
 using Business.MissionCategoryManagement.Dto;
@@ -22,7 +24,10 @@ public class ReportAppService : ApplicationService, IReportAppService
     private readonly (
         IRepository<Language> Langugage,
         IRepository<MissionOverAllView> MissionOverAllView,
-        IRepository<MissionCategoryView> MissionCategoryView
+        IRepository<MissionCategoryView> MissionCategoryView,
+        IRepository<MissionView> MissionView,
+        IRepository<Language> Language,
+        IRepository<LocalizationText> LocalizationText
         ) _repositorys;
 
     private readonly ILogger<MissionAppService> _logger;
@@ -31,9 +36,12 @@ public class ReportAppService : ApplicationService, IReportAppService
         IRepository<Language> Langugage,
         IRepository<MissionOverAllView> MissionOverAllView,
         IRepository<MissionCategoryView> MissionCategoryView,
+        IRepository<MissionView> MissionView,
+        IRepository<Language> Language,
+        IRepository<LocalizationText> LocalizationText,
         ILogger<MissionAppService> logger)
     {
-        _repositorys = (Langugage, MissionOverAllView, MissionCategoryView);
+        _repositorys = (Langugage, MissionOverAllView, MissionCategoryView, MissionView, Language, LocalizationText);
         _logger = logger;
     }
 
@@ -43,48 +51,56 @@ public class ReportAppService : ApplicationService, IReportAppService
     public async Task<MyFileInfoDto> GetFinishRateReport(List<Guid> ids ,Guid? teamId, string name, string code)
     {
         var workbook = new XLWorkbook();
-        var worksheet = workbook.AddWorksheet($"{name}的任務完成度報告");
+        var sheetName = (await _repositorys.LocalizationText.GetAsync(x => x.LanguageCode == code &&
+                            x.Category == "SheetName" && x.ItemKey == "11")).ItemValue;
+        var worksheet = workbook.AddWorksheet($"{name}${sheetName}");
 
-        var lang = 1;
-        var language = await _repositorys.Langugage.FindAsync(x => x.Code == code);
-        lang = !lang.IsNullOrEmpty() ? language.Id : lang;
+        var queryLocalization = await _repositorys.LocalizationText.GetQueryableAsync();
+        var isFinishMap = queryLocalization.Where(x => x.LanguageCode == code && x.Category == "MissionState")
+            .ToDictionary(x => x.ItemKey, x => x.ItemValue);
+        var language = await _repositorys.Langugage.GetAsync(x => x.Code == code);
 
-        // 獲取任務overAllView
+        /// 獲取任務overAllView
         var missions = await _repositorys.MissionOverAllView
-            .GetListAsync(x => x.TeamId == teamId && x.Lang == lang && ids.Contains(x.SubCategoryId));
+            .GetListAsync(x => x.TeamId == teamId && x.Lang == language.Id && ids.Contains(x.SubCategoryId));
 
         var startColumn = 1;
         var startRow = 1;
         var endRow = 1;
         var endColumn = 9;
 
-        // 設定標頭團隊名稱
+        /// 設定標頭團隊名稱
         worksheet.Range(startRow, startColumn, startRow, endColumn).Merge();
-        // 設背景顏色
+        /// 設背景顏色
         worksheet.Cell(startRow, startColumn).Style.Fill.BackgroundColor = XLColor.Yellow;
-        // 水平對齊
+        /// 水平對齊
         worksheet.Cell(startRow, startColumn).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
         worksheet.Cell(startRow++, startColumn).Value = $"{name}";
 
-        // 設定標頭團隊名稱
+        /// 設定標頭團隊名稱
         worksheet.Range(startRow, startColumn, startRow + missions.Count, startColumn + 1).Merge();
-        // 設背景顏色
+        /// 設背景顏色
         worksheet.Cell(startRow, startColumn).Style.Fill.BackgroundColor = XLColor.Red;
-        // 水平對齊
+        /// 水平對齊
         worksheet.Cell(startRow, startColumn).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
         worksheet.Cell(startRow, startColumn++).Value = $"{name}";
 
-        // 寫入Title
+        /// 寫入Title
         int titleStartColumn = startColumn;
         var titles = new List<string> { "類別", "子類別", "父任務", "是否完成", "子任務", "是否完成", "父任務完成狀態(%)" };
-        // 設背景顏色
-        worksheet.Range(startRow, ++startColumn, startRow, endColumn).Style.Fill.BackgroundColor = XLColor.Blue;
+        /// 設背景顏色
+        worksheet.Range(startRow, ++startColumn, startRow, endColumn).Style.Fill.BackgroundColor = XLColor.BallBlue;
         foreach (var title in titles)
         {
             worksheet.Cell(startRow, ++titleStartColumn).Value = title;
         }
 
         startRow++;
+
+        /// 計算子任務數量來計算出父任務是否完成(是 -> 下面子任務全完成)
+        var queryMission = await _repositorys.MissionView.GetQueryableAsync();
+        var isMissionFinishMap = queryMission.Where(x => ids.Contains(x.MissionCategoryId) && x.ParentMissionId != null && !string.IsNullOrEmpty(x.MissionName))
+            .GroupBy(x => x.ParentMissionId).ToDictionary(g => g.Key, x => x.All(y => y.MissionFinishTime != null));
 
         var startDataColumn = startColumn;
         for (int i = 0; i < missions.Count; i++)
@@ -100,8 +116,16 @@ public class ReportAppService : ApplicationService, IReportAppService
 
                 if (property.PropertyType == typeof(DateTime?))
                 {
-                    worksheet.Cell(startRow, startDataColumn++).Value =
-                        property.GetValue(missions[i]).IsNullOrEmpty() ? "否" : "是";
+                    /// 0 -> 否 , 1 -> 是
+                    string isFinish = property.Name == "MissionFinishTime" 
+                        ? isMissionFinishMap[missions[i].MissionId] ? isFinishMap["1"] : isFinishMap["0"]
+                        : property.GetValue(missions[i]).IsNullOrEmpty() ? isFinishMap["0"] : isFinishMap["1"];
+                    if(property.Name == "MissionFinishTime")
+                    {
+                        var b = isMissionFinishMap[missions[i].MissionId] ? isFinishMap["1"] : isFinishMap["0"];
+                        _logger.LogError($"==================={b}");
+                    }
+                    worksheet.Cell(startRow, startDataColumn++).Value = $"{isFinish}";
                 }
                 else
                 {
@@ -116,7 +140,9 @@ public class ReportAppService : ApplicationService, IReportAppService
         using var memoryStream = new MemoryStream();
         workbook.SaveAs(memoryStream);
 
-        return new MyFileInfoDto { FileContent = memoryStream.ToArray(), FileName = $"{name}的任務完成度報告.xlsx" };
+        var fileNmae = (await _repositorys.LocalizationText.GetAsync(x => x.LanguageCode == code &&
+                            x.Category == "Export" && x.ItemKey == "11")).ItemValue;
+        return new MyFileInfoDto { FileContent = memoryStream.ToArray(), FileName = $"{name}{fileNmae}" };
     }
 
     /// <summary>
