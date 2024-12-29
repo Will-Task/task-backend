92270ea7c418631593f8d946b80da506300e90c6
Jackwio <will582145@gmail.com>
2024-12-29 19:52:11 +0800

    完成度報告 增加父任務完成率計算

--- MicroServices/Business/Business.Application/ReportManagement/ReportAppService.cs
+++ MicroServices/Business/Business.Application/ReportManagement/ReportAppService.cs
@@ -53,7 +53,8 @@ public class ReportAppService : ApplicationService, IReportAppService
         ILogger<MissionAppService> logger,
         IStringLocalizer<BusinessResource> localizer)
     {
-        _repositorys = (Langugage, MissionOverAllView, MissionCategoryView, MissionView, Language, LocalizationText, MissionI18N, MissionCategoryI18N);
+        _repositorys = (Langugage, MissionOverAllView, MissionCategoryView, MissionView, Language, LocalizationText,
+            MissionI18N, MissionCategoryI18N);
         _logger = logger;
         _localizer = localizer;
     }
@@ -61,11 +62,12 @@ public class ReportAppService : ApplicationService, IReportAppService
     /// <summary>
     /// 匯出任務完成度報告
     /// </summary>
-    public async Task<MyFileInfoDto> GetFinishRateReport(List<Guid> ids ,Guid? teamId, string name, string code)
+    public async Task<MyFileInfoDto> GetFinishRateReport(List<Guid> ids, Guid? teamId, string name, string code)
     {
         var workbook = new XLWorkbook();
         var sheetName = (await _repositorys.LocalizationText.GetAsync(x => x.LanguageCode == code &&
-                            x.Category == "SheetName" && x.ItemKey == "11")).ItemValue;
+                                                                           x.Category == "SheetName" &&
+                                                                           x.ItemKey == "11")).ItemValue;
         var worksheet = workbook.AddWorksheet($"{name}${sheetName}");
 
         var queryLocalization = await _repositorys.LocalizationText.GetQueryableAsync();
@@ -82,38 +84,41 @@ public class ReportAppService : ApplicationService, IReportAppService
         var queryMissionI18N = await _repositorys.MissionI18N.GetQueryableAsync();
         var defaultMissionMap = queryMissionI18N.OrderBy(x => x.Lang == language.Id ? 0 : x.Lang)
             .OrderBy(x => x.Lang).GroupBy(x => x.MissionId)
-            .ToDictionary(g => g.Key, x => x.First().MissionName );
-    
+            .ToDictionary(g => g.Key, x => x.First().MissionName);
+
         var queryCategoryI18N = await _repositorys.MissionCategoryI18N.GetQueryableAsync();
         var defaultCategoryMap = queryCategoryI18N.OrderBy(x => x.Lang == language.Id ? 0 : x.Lang)
             .OrderBy(x => x.Lang).GroupBy(x => x.MissionCategoryId)
-            .ToDictionary(g => g.Key, x => x.First().MissionCategoryName );
+            .ToDictionary(g => g.Key, x => x.First().MissionCategoryName);
 
         /// 獲取任務overAllView
         var missions = await _repositorys.MissionOverAllView
             .GetListAsync(x => x.TeamId == teamId && x.Lang == language.Id && ids.Contains(x.SubCategoryId));
         var dtos = ObjectMapper.Map<List<MissionOverAllView>, List<MissionOverAllViewDto>>(missions);
-        
+
         foreach (var dto in dtos)
         {
             if (dto.MissionName.IsNullOrEmpty())
             {
                 dto.MissionName = defaultMissionMap[dto.MissionId];
             }
+
             if (dto.SubMissionName.IsNullOrEmpty())
             {
                 dto.SubMissionName = defaultMissionMap[dto.MissionId];
             }
+
             if (dto.CategoryName.IsNullOrEmpty())
             {
                 dto.CategoryName = defaultCategoryMap[dto.CategoryId];
             }
+
             if (dto.SubCategoryName.IsNullOrEmpty())
             {
                 dto.SubCategoryName = defaultCategoryMap[dto.CategoryId];
             }
         }
-        
+
         var exportDtos = ObjectMapper.Map<List<MissionOverAllViewDto>, List<ExportMissionOverAllViewDto>>(dtos);
 
         var startColumn = 1;
@@ -139,9 +144,13 @@ public class ReportAppService : ApplicationService, IReportAppService
 
         /// 寫入Title
         int titleStartColumn = startColumn;
-        var titles = new List<string> { $"{_localizer["FinishReport:Column:Category"]}", $"{_localizer["FinishReport:Column:SubCategory"]}", $"{_localizer["FinishReport:Column:Mission"]}",
-                                          $"{_localizer["FinishReport:Column:isFinish"]}", $"{_localizer["FinishReport:Column:SubMission"]}", $"{_localizer["FinishReport:Column:isFinish"]}"
-                                          , $"{_localizer["FinishReport:Column:MissionFinishRate"]}" };
+        var titles = new List<string>
+        {
+            $"{_localizer["FinishReport:Column:Category"]}", $"{_localizer["FinishReport:Column:SubCategory"]}",
+            $"{_localizer["FinishReport:Column:Mission"]}",
+            $"{_localizer["FinishReport:Column:isFinish"]}", $"{_localizer["FinishReport:Column:SubMission"]}",
+            $"{_localizer["FinishReport:Column:isFinish"]}", $"{_localizer["FinishReport:Column:MissionFinishRate"]}"
+        };
         /// 設背景顏色
         worksheet.Range(startRow, ++startColumn, startRow, endColumn).Style.Fill.BackgroundColor = XLColor.BallBlue;
         foreach (var title in titles)
@@ -153,8 +162,13 @@ public class ReportAppService : ApplicationService, IReportAppService
 
         /// 計算子任務數量來計算出父任務是否完成(是 -> 下面子任務全完成)
         var queryMission = await _repositorys.MissionView.GetQueryableAsync();
-        var isMissionFinishMap = queryMission.Where(x => ids.Contains(x.MissionCategoryId) && x.ParentMissionId != null && !string.IsNullOrEmpty(x.MissionName))
+        var isMissionFinishMap = queryMission.Where(x =>
+                ids.Contains(x.MissionCategoryId) && x.ParentMissionId != null && !string.IsNullOrEmpty(x.MissionName))
             .GroupBy(x => x.ParentMissionId).ToDictionary(g => g.Key, x => x.All(y => y.MissionFinishTime != null));
+
+        var subMissionFinishMap = queryMission.Where(x =>
+                ids.Contains(x.MissionCategoryId) && x.ParentMissionId != null && !string.IsNullOrEmpty(x.MissionName))
+            .GroupBy(x => x.ParentMissionId).ToDictionary(g => g.Key);
 
         var startDataColumn = startColumn;
         foreach (var exportDto in exportDtos)
@@ -170,13 +184,16 @@ public class ReportAppService : ApplicationService, IReportAppService
                 if (property.PropertyType == typeof(DateTime?))
                 {
                     /// 0 -> 否 , 1 -> 是
-                    string isFinish = property.Name == "MissionFinishTime" 
+                    string isFinish = property.Name == "MissionFinishTime"
                         ? isMissionFinishMap[exportDto.MissionId] ? isFinishMap["1"] : isFinishMap["0"]
-                        : property.GetValue(exportDto).IsNullOrEmpty() ? isFinishMap["0"] : isFinishMap["1"];
-                    if(property.Name == "MissionFinishTime")
+                        : property.GetValue(exportDto).IsNullOrEmpty()
+                            ? isFinishMap["0"]
+                            : isFinishMap["1"];
+                    if (property.Name == "MissionFinishTime")
                     {
                         var b = isMissionFinishMap[exportDto.MissionId] ? isFinishMap["1"] : isFinishMap["0"];
                     }
+
                     worksheet.Cell(startRow, startDataColumn++).Value = $"{isFinish}";
                 }
                 else
@@ -185,6 +202,10 @@ public class ReportAppService : ApplicationService, IReportAppService
                 }
             }
 
+            int total = subMissionFinishMap[exportDto.MissionId].Count();
+            int finish = subMissionFinishMap[exportDto.MissionId].Count(x => x.MissionFinishTime != null);
+            decimal rate = (decimal)(finish * 100) / total;
+            worksheet.Cell(startRow, startDataColumn++).Value = $"{Math.Round(rate, 3)}%";
             startRow++;
             startDataColumn = startColumn;
         }
@@ -193,7 +214,8 @@ public class ReportAppService : ApplicationService, IReportAppService
         workbook.SaveAs(memoryStream);
 
         var fileNmae = (await _repositorys.LocalizationText.GetAsync(x => x.LanguageCode == code &&
-                            x.Category == "Export" && x.ItemKey == "11")).ItemValue;
+                                                                          x.Category == "Export" && x.ItemKey == "11"))
+            .ItemValue;
         return new MyFileInfoDto { FileContent = memoryStream.ToArray(), FileName = $"{name}{fileNmae}" };
     }
 
@@ -207,12 +229,12 @@ public class ReportAppService : ApplicationService, IReportAppService
         var defaultCategoryMap = query.OrderBy(x => x.Lang == 1 ? 0 : x.Lang)
             .OrderBy(x => x.Lang).GroupBy(x => x.MissionCategoryId)
             .ToDictionary(g => g.Key, x => x.First().MissionCategoryName);
-        
+
         /// 取得父任務和子任務同筆的數據
         var categories = await _repositorys.MissionCategoryView
             .GetListAsync(x => x.TeamId == teamId && x.ParentId != null);
         var dtos = ObjectMapper.Map<List<MissionCategoryView>, List<MissionCategoryViewDto>>(categories);
-        
+
         foreach (var dto in dtos)
         {
             if (dto.MissionCategoryName.IsNullOrEmpty())
